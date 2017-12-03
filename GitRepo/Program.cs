fe33e63c4a288dfb65468fb615177775c06503a9
DaniAwan <danizia.awan@gmail.com>
2017-12-03 12:52:30 -0800

    added

--- GitRepo/Program.cs
+++ GitRepo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GitRepo
 {
@@ -10,6 +6,7 @@ namespace GitRepo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Good");
         }
     }
 }
